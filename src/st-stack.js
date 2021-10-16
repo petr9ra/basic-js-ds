fdb8a9ee18f5184ec33da0c342c40ce3c5035278
Petr <petr9ra@yandex.ru>
2021-10-16 17:18:28 +0300

    feat: add solution for the stack task

--- src/st-stack.js
+++ src/st-stack.js
@@ -12,19 +12,20 @@ const { NotImplementedError } = require('../extensions/index.js');
  * stack.pop(); // undefined
  *
  */
- module.exports = class Stack {
-  push(/* element */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+module.exports = class Stack {
+  constructor() {
+    this.stack = [];
+  }
+
+  push(data) {
+    return this.stack.push(data);
   }
 
   pop() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.stack.pop();
   }
 
   peek() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.stack[this.stack.length - 1];
   }
 }
