9ac8bf9e197c6af511094a922ad9e7442c7eb24c
Petr <petr9ra@yandex.ru>
2021-10-16 18:01:23 +0300

    feat: add solution for the remove-from-list task

--- src/st-remove-from-list.js
+++ src/st-remove-from-list.js
@@ -19,7 +19,20 @@ const { NotImplementedError } = require('../extensions/index.js');
  * }
  */
 
-module.exports = function removeKFromList(/* l, k */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function removeKFromList(list, k) {
+  let curr = list;
+  while (curr.value === k) curr = curr.next;
+  list = curr;
+
+  if (curr) {
+    let prev = null;
+    while (curr.next) {
+      prev = curr;
+      curr = curr.next;
+      if (curr.value === k) {
+        prev.next = curr.next;
+      }
+    }
+    return list;
+  }
 }
